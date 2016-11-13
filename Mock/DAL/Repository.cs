9c588de2d3893377b7b8f874b2a11134f3e6d67f
zsolti111 <nemzsolti@gmail.com>
2016-11-13 14:51:20 +0100

    Szétszedtem külön a stub miatt

---
+++ Mock/DAL/Repository.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+
+namespace DAL
+{
+    public class Repository : IDisposable
+    {
+
+        /// <summary>
+        /// Path to the .csv file's directory
+        /// </summary>
+        string path = Directory.GetCurrentDirectory();
+
+        StreamWriter streamCSV = null;
+
+        public Repository()
+        {
+
+        }
+
+        static void Main(string[] args)
+         {
+
+        }
+
+
+        public void Logger()
+        {
+            /* Create the directory if it not exist */
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+
+            //-------------------------------------
+            //---------- CSV logging---------------
+            //-------------------------------------
+
+            try
+            {
+                // If it's the first time we want to write
+                if (streamCSV == null)
+                {
+
+                    streamCSV = new StreamWriter(new FileStream(path + "MyFile" + ".csv", FileMode.Append, FileAccess.Write, FileShare.ReadWrite));
+
+                    /* If the file is empty we make the header */
+                    var csvFileLenth = new System.IO.FileInfo(path + "MyFile" + ".csv").Length;
+
+                    if (csvFileLenth == 0)
+                    {
+                        streamCSV.AutoFlush = true;
+                        streamCSV.Write("AAA" + Environment.NewLine +
+                                        "BBB" + Environment.NewLine);
+
+                    }
+
+                    streamCSV.Dispose();
+
+
+                }
+
+
+
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Stop the Logging, close the Stream
+        /// </summary>
+        public void StopLogTradeHistory()
+        {
+            if (streamCSV != null)
+            {
+                streamCSV.Dispose();
+            }
+        }
+
+
+        /// <summary>
+        /// Load the existing Trade History
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>    
+        public string Load(string path)
+        {
+
+            /* Ha nincs file, ne szálljon el Load-nál */
+            if (File.Exists(path) == false)
+            {
+                string ret = "nincs file";
+                return ret;
+            }
+
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(File.OpenRead(path));
+                string line = "";
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+
+                }
+
+                /* Free the reader */
+                reader.Dispose();
+                return line;
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                string exc = "exception";
+                return exc;
+            }
+
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Dispose();
+                }
+
+            }
+
+        }
+
+
+        // DISPOSE //
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (streamCSV != null)
+                        streamCSV.Dispose();
+                }
+                // Release unmanaged resources.
+                disposed = true;
+            }
+        }
+
+        ~Repository() { Dispose(false); }
+
+
+
+
+
+    }
+}
+
+    
+
